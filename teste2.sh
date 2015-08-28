d9acf2d7921cdf57783b1247c783e8dbefba5399
thoth <thoth@athos>
2015-08-28 00:03:12 -0300

    commit inicial v2

---
+++ teste2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-7-oracle/bin:/usr/lib/jvm/java-7-oracle/db/bin:/usr/lib/jvm/java-7-oracle/jre/bin
+echo "Execução de script de inicialização"
+sleep 1
+echo "Preparação para o hardening do servidor"
+sleep 1
+echo "Executando atualização do sistema"
+sudo apt-get update
+
